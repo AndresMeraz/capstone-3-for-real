5c9c70b30608dc900bdbe5afad90738444c4a78f
AndresMeraz <andres.meraz312@gmail.com>
2023-12-21 15:29:27 -0800

    finished registration

--- js/registration.js
+++ js/registration.js
@@ -7,31 +7,36 @@ function init(){
 }
 
 async function registerNewUser(event) {
-    event.preventDefault();
-    class user{
-        constructor(username, fullName, password){
-        this.username = username;
-        this.fullName = fullName
-        this.password = password;
-        }
-    }
-
-    const newUser = new UserActivation(document.getElementById("userName").value, document.getElementById("fullName").value, document.getElementById("password").value)
+    event.preventDefault()
     
-    try {
-        const response = await fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
-            method:'POST',
-            headers: {
-                "Accept": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTcwMzE3NzA0NiwiZXhwIjoxNzAzMjYzNDQ2fQ.aMFLOT7Yj9T_YwcVNrY7Tc-iSlqafB1nT0yzgucSsGM",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newUser)
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    
+    if (password != confirmPassword) {
+        alert('Passwords mismatch!');
+        // Add your form submission logic here
+    } 
+    else {
+        try {
+            const response = await fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
+                method:'POST',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: document.getElementById("userName").value,
+                    fullName: document.getElementById("fullName").value,
+                    password: document.getElementById("password").value,
+                })
             })
-        const data = await response.json();  
-        console.log(`User ${user} added succesfully`)
-    }
+            const data = await response.json();  
+            alert(`User added succesfully`)
+            console.log(data);
+            window.location.href = "login.html"
+        }
         catch (error) {
             console.error('Error', error)
         }
     }
+}
