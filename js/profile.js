c0799ac311c73ca1fa1a5e721682f3cabd81098f
Justine Elbuhel <nineplusfour.13@gmail.com>
2024-01-03 15:45:13 -0800

    Trainer Image

--- js/profile.js
+++ js/profile.js
@@ -156,13 +156,6 @@ getProfileInfo();
         return img;
     }
 
-// //todo pulling filepath for randomizer algo
-//     function pokePics() {
-//         path = URL("pokehashLibs/pokemons/pokemon" + num + ".png")
-//         let num = 
-//         for (let j = 0; j < 8; j++) {
-//     }
-
 //TODO pokemon trainer output
 function displayProfile(data){
     console.log(data);
@@ -223,7 +216,7 @@ function getRegion(region){
 
 
 function draw(ctx) {
-    //?coordinates based on card dimensions
+    //coordinates based on card dimensions
     let pokegridW = 28;
     let pokegridH = 222;
 
@@ -233,6 +226,7 @@ function draw(ctx) {
     const loginData = getLoginData();
     let userCard = CreateTrainer(loginData.username);
     console.log(userCard.pokemon1)
+
     // adding pokemon
         let pokemon1 = loadImage("pokehashLibs/pokemons/pokemon" + userCard.pokemon1 + ".png");
         pokemon1.onload = function() {
@@ -264,6 +258,12 @@ function draw(ctx) {
         pokemon6.onload = function() {
             ctx.drawImage(pokemon6, pokegridW + (offsetW*2), pokegridH + (offsetH));
         };
+    
+    // adding trainer image
+        let trainerImg = loadImage("pokehashLibs/trainers/trainer" + userCard.trainer + ".png");
+        trainerImg.onload = function() {
+            ctx.drawImage(trainerImg, 260, 170 )
+        }
 
     //? adding badges
     let horizontal = 52;
@@ -426,129 +426,3 @@ function CreateTrainer(name) {
     
         return result;
     };
-    
-                // function CreateTrainer(name) {
-                //     console.log(name)
-                //     const createIDcalc = createID();
-                //     const cashMoney = getThatBag();
-                //     const pokedexCalc = getPokedex();
-            
-                //     var hash = sha256(name);
-                
-                //     trainercard.name		= name;
-                //     // trainercard.region		= regions[	(parseInt('0x' + hash.substring(0, 4)) % 2)];
-                //     // trainercard.hometown	= cities[		(parseInt('0x' + hash.substring(0, 4)) % 20)];
-                //     // trainercard.money		= 			(parseInt('0x' + cashMoney.substring(5, 9)));
-                //     trainercard.pokedex		= 			(parseInt('0x' + pokedexCalc.substring(10, 14)) % 252);
-                //     trainercard.badges		= 			(parseInt('0x' + hash.substring(15, 19)) % 9);
-                //     trainercard.trainer		= 			(parseInt('0x' + hash.substring(20, 24)) % 107);
-                //     trainercard.pokemon1	= 			(parseInt('0x' + hash.substring(25, 29)) % 252);
-                //     trainercard.pokemon2	= 			(parseInt('0x' + hash.substring(30, 34)) % 252);
-                //     trainercard.pokemon3	= 			(parseInt('0x' + hash.substring(35, 39)) % 252);
-                //     trainercard.pokemon4	= 			(parseInt('0x' + hash.substring(40, 44)) % 252);
-                //     trainercard.pokemon5	= 			(parseInt('0x' + hash.substring(45, 49)) % 252);
-                //     trainercard.pokemon6	= 			(parseInt('0x' + hash.substring(50, 54)) % 252);
-                //     trainercard.id 		= 			(parseInt('0x' + createIDcalc.length(55, 59)));
-                
-                //     return trainercard;
-                // }
-                        
-                // var sha256 = function sha256(ascii) {
-                //     function rightRotate(value, amount) {
-                //         return (value>>>amount) | (value<<(32 - amount));
-                //     };
-                
-                //     var mathPow = Math.pow;
-                //     var maxWord = mathPow(2, 32);
-                //     var lengthProperty = 'length'
-                //     var i, j; // Used as a counter across the whole file
-                //     var result = ''
-                
-                //     var words = [];
-                //     var asciiBitLength = ascii[lengthProperty]*8;
-                
-                //     //* caching results is optional - remove/add slash from front of this line to toggle
-                //     // Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
-                //     // (we actually calculate the first 64, but extra values are just ignored)
-                //     var hash = sha256.h = sha256.h || [];
-                //     // Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
-                //     var k = sha256.k = sha256.k || [];
-                //     var primeCounter = k[lengthProperty];
-                //     /*/
-                //     var hash = [], k = [];
-                //     var primeCounter = 0;
-                //     //*/
-                
-                //     var isComposite = {};
-                //     for (var candidate = 2; primeCounter < 64; candidate++) {
-                //         if (!isComposite[candidate]) {
-                //             for (i = 0; i < 313; i += candidate) {
-                //                 isComposite[i] = candidate;
-                //             }
-                //             hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
-                //             k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
-                //         }
-                //     }
-                
-                //     ascii += '\x80' // Append Ƈ' bit (plus zero padding)
-                //     while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
-                //     for (i = 0; i < ascii[lengthProperty]; i++) {
-                //         j = ascii.charCodeAt(i);
-                //         if (j>>8) return; // ASCII check: only accept characters in range 0-255
-                //         words[i>>2] |= j << ((3 - i)%4)*8;
-                //     }
-                //     words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
-                //     words[words[lengthProperty]] = (asciiBitLength)
-                
-                //     // process each chunk
-                //     for (j = 0; j < words[lengthProperty];) {
-                //         var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
-                //         var oldHash = hash;
-                //         // This is now the undefinedworking hash", often labelled as variables a...g
-                //         // (we have to truncate as well, otherwise extra entries at the end accumulate
-                //         hash = hash.slice(0, 8);
-                
-                //         for (i = 0; i < 64; i++) {
-                //             var i2 = i + j;
-                //             // Expand the message into 64 words
-                //             // Used below if
-                //             var w15 = w[i - 15], w2 = w[i - 2];
-                
-                //             // Iterate
-                //             var a = hash[0], e = hash[4];
-                //             var temp1 = hash[7]
-                //                 + (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
-                //                 + ((e&hash[5])^((~e)&hash[6])) // ch
-                //                 + k[i]
-                //                 // Expand the message schedule if needed
-                //                 + (w[i] = (i < 16) ? w[i] : (
-                //                         w[i - 16]
-                //                         + (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
-                //                         + w[i - 7]
-                //                         + (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
-                //                     )|0
-                //                 );
-                //             // This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
-                //             var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
-                //                 + ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
-                
-                //             hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
-                //             hash[4] = (hash[4] + temp1)|0;
-                //         }
-                
-                //         for (i = 0; i < 8; i++) {
-                //             hash[i] = (hash[i] + oldHash[i])|0;
-                //         }
-                //     }
-                
-                
-                //     for (i = 0; i < 8; i++) {
-                //         for (j = 3; j + 1; j--) {
-                //             var b = (hash[i]>>(j*8))&255;
-                //             result += ((b < 16) ? 0 : '') + b.toString(16);
-                //         }
-                //     }
-                
-                //     return result;
-                // };
-                
