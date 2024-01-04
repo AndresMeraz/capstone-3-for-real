6e404bb0266fadf08ce63a8fd1770d062a1a6ff9
EWAjayi <e.ajayi97@gmail.com>
2024-01-04 16:43:38 -0500

    new header + updated profile page

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
 "use strict"
-
+//creating canvas for trainer card
 window.onload = function() {
     //placing the card
     const canvas = document.getElementById("trainerCanvas");
@@ -7,11 +7,11 @@ window.onload = function() {
     const img = document.getElementById("trainerCard");
     ctx.drawImage(img, 0, 0);
     draw(ctx);
-    textFont(myFont);
 };
 
+//structure of trainercard
 const trainercard = [{
-	'id': '',
+    'id': '',
     'name': '',
 	'region': '',
 	'hometown': '',
@@ -27,138 +27,153 @@ const trainercard = [{
 	'pokemon6': ''
 }]
 
-// preload
+// arrays for images
+let trainer = [];
+var pokemon = [];
+var badge = [];
+
+// initializinf variables
+let myFont;
+let card;
+
+//pulling file location for images
+function loadImage(src) {
+    let img = new Image();
+    img.src = src;
+    return img;
+}
+
+// preloading images
 function preload() {
-
-	// Load trainer images
-	for (i = 0; i < 107; i++){
-		trainer[i] = loadImage('trainers/trainer' + i + '.png');
+    
+    // Load trainer images
+	for (let i = 0; i < 107; i++){
+        trainer[i] = loadImage('./pokehasLibs/trainers/trainer' + i + '.png');
 	}
-
+    
 	// Load pokemon images
-	for (i = 0; i < 252; i++){
-		pokemon[i] = loadImage('pokemons/pokemon' + i + '.png')
+	for (let i = 0; i < 252; i++){
+        pokemon[i] = loadImage('./pokehasLibs/pokemons/pokemon' + i + '.png')
 	}
 
 	// Load Kanto badges
-	for(i = 0; i < 8; i++){
-		badge[i] = loadImage('badges/kanto/kanto' + i + '.png');
+	for(let i = 0; i < 8; i++){
+		badge[i] = loadImage('./pokehasLibs/badges/kanto/kanto' + i + '.png');
 	}
-
+    
 	// Load Johto badges
-	for(i = 8; i < 16; i++){
-		badge[i] = loadImage('badges/johto/johto' + i + '.png');
+	for(let i = 8; i < 16; i++){
+        badge[i] = loadImage('./pokehasLibs/badges/johto/johto' + i + '.png');
 	}
-
-	// Load Font
-	myFont = loadFont("pokehashKLibs/pokedex.ttf");
-
-}
     
-function getLoginData(){
-    const loginJSON = window.localStorage.getItem("login-data");
-    return JSON.parse(loginJSON) || {};
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const loginData = getLoginData();
-    // Fetch posts from the API endpoint
-    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=100&offset=0", {
-        method: "GET",
-        headers: {
-            "Accept": "application/json",
-            "Authorization": `Bearer ${loginData.token}`,
-        }
-    })
-    .then(response => response.json())
-    .then(posts => findMatching(posts))
-    .catch(error => {
-        console.error("Error fetching data:", error);
-    })})
+//*enabiling login and post functionality
+    function getLoginData(){
+        const loginJSON = window.localStorage.getItem("login-data");
+        return JSON.parse(loginJSON) || {};
+    }
 
-    
-    function findMatching(posts){
-        const postList = document.getElementById("postList");
+    document.addEventListener("DOMContentLoaded", function () {
         const loginData = getLoginData();
-        let username = loginData.username;
-        // console.log(username);
-        
-        // Loop through the posts and display them
-        let matching = posts.filter(user => user.username == username)
-    // console.log(matching);
-    
-    matching.forEach(matching => {
-        const postCard = document.createElement("div");
-        postCard.classList.add("card", "mb-3");
-        
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        
-        const postText = document.createElement("p");
-        postText.classList.add("card-text");
-        postText.textContent = matching.text;
-        
-        const postUsername = document.createElement("p");
-        postUsername.classList.add("card-text", "text-muted");
-        postUsername.textContent = `Posted by ${matching.username} on ${new Date(matching.createdAt).toLocaleString()}`;
-
-        const deletePost = document.createElement("button");
-        deletePost.classList.add("deleteBtn");
-        deletePost.id = `${matching._id}`;
-        deletePost.textContent = "Delete"
-        deletePost.addEventListener("click", function(event){
-            let postID = event.currentTarget.id;
-            console.log(postID)
-            deleteThisPost(postID)
+        // Fetch posts from the API endpoint
+        fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=100&offset=0", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${loginData.token}`,
+            }
         })
+        .then(response => response.json())
+        .then(posts => findMatching(posts))
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        })})
 
         
-        cardBody.appendChild(postText);
-        cardBody.appendChild(postUsername);
-        cardBody.appendChild(deletePost);
-        postCard.appendChild(cardBody);
+        function findMatching(posts){
+            const postList = document.getElementById("postList");
+            const loginData = getLoginData();
+            let username = loginData.username;
+            // console.log(username);
+            
+            // Loop through the posts and display them
+            let matching = posts.filter(user => user.username == username)
+            // console.log(matching);
         
-        postList.appendChild(postCard);
-    });
-}
+            matching.forEach(matching => {
+                const postCard = document.createElement("div");
+                postCard.classList.add("card", "mb-3");
+                
+                const cardBody = document.createElement("div");
+                cardBody.classList.add("card-body");
+                
+                const postText = document.createElement("p");
+                postText.classList.add("card-text");
+                postText.textContent = matching.text;
+                
+                const postUsername = document.createElement("p");
+                postUsername.classList.add("card-text", "text-muted");
+                postUsername.textContent = `Posted by ${matching.username} on ${new Date(matching.createdAt).toLocaleString()}`;
+                
+                const deletePost = document.createElement("button");
+                deletePost.classList.add("deleteBtn");
+                deletePost.id = `${matching._id}`;
+                deletePost.textContent = "Delete"
+                deletePost.addEventListener("click", function(event){
+                    let postID = event.currentTarget.id;
+                    console.log(postID)
+                    deleteThisPost(postID)
+                })
 
-function deleteThisPost(postID){
-    let postId = postID;
-    const loginData = getLoginData();
-    console.log(postId)
-    let baseURL = `http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/${postId}`
-    fetch(baseURL, {
-        method: 'DELETE',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-            "Authorization": `Bearer ${loginData.token}`,
-        },
-    })
-    .then((response) => response.json())
-    .then((data) => console.log("Post Deleted Successfully"))
-    .catch((error) => console.error(error))
-}
+                
+                cardBody.appendChild(postText);
+                cardBody.appendChild(postUsername);
+                cardBody.appendChild(deletePost);
+                postCard.appendChild(cardBody);
+                
+                postList.appendChild(postCard);
+        });
+    }
 
-function getProfileInfo() {
-    const loginData = getLoginData();
-    // let profile = document.getElementById("profile");
-    // console.log(loginData);
-    let profileName = loginData.username;
-    // profile.innerHTML = profileName;
+    function deleteThisPost(postID){
+        let postId = postID;
+        const loginData = getLoginData();
+        console.log(postId)
+        let baseURL = `http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/${postId}`
+        fetch(baseURL, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                "Authorization": `Bearer ${loginData.token}`,
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => console.log("Post Deleted Successfully"))
+        .catch((error) => console.error(error))
+    }
+
+    function getProfileInfo() {
+        const loginData = getLoginData();
+        // let profile = document.getElementById("profile");
+        // console.log(loginData);
+        let profileName = loginData.username;
+        // profile.innerHTML = profileName;
+        
+        let baseURL = `http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${profileName}`;
+        
+        fetch(baseURL,{
+            method: "GET",
+            headers: { 
+                Authorization: `Bearer ${loginData.token}`,
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => displayProfile(data))
+    }
+
+    getProfileInfo();
     
-    let baseURL = `http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${profileName}`;
-    
-    fetch(baseURL,{
-        method: "GET",
-        headers: { 
-            Authorization: `Bearer ${loginData.token}`,
-        }
-    })
-    .then((response) => response.json())
-    .then((data) => displayProfile(data))
-}
-
-getProfileInfo();
 //todo math algorithims for randomizing numbers 
     function createID(min, max) {
         min = 1000;
@@ -177,84 +192,94 @@ getProfileInfo();
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function loadImage(src) {
-        let img = new Image();
-        img.src = src;
-        return img;
+
+//TODO user information output
+    function displayProfile(data){
+        console.log(data);
+
+        const userName = data.username;
+        console.log(userName);
+
+        const region = getRegion(data.bio);   //replaced region designation with user input
+        console.log(region);
+
+        const id = CreateTrainer(userName);
+        console.log(id.id);
+
+        const money = getThatBag();
+        console.log(money);
+
+        const pokedex = getPokedex();
+        console.log(pokedex);
+
+        //targeting location on html
+        const canvas = document.getElementById("trainerCanvas");
+        const ctx = canvas.getContext("2d");
+
+        //loading font
+        const pokeFont = new FontFace("pokeFont", "url(./pokehashLibs/pokedex.ttf)");
+        pokeFont.load().then(function(font) {
+            document.fonts.add(font);
+            console.log('Font loaded');
+
+            // adding text
+            ctx.font = "16px pokeFont";
+            ctx.fillText("IDNo. " + id.id, 263, 42);
+            ctx.fillText("NAME: " + userName, 45, 84);
+            ctx.fillText("REGION: " + region, 45, 138);
+            ctx.fillText("MONEY: $" + money, 45, 170);
+            ctx.fillText("POKEDéX: " + pokedex, 45, 202);
+        })
+
+    }
+    function getRegion(region){
+        if(region == "Kanto"){
+            return "Kanto"
+        }
+        else if(region == "Johto"){
+            return "Johto"
+        }
+        else if(region == "Hoenn"){
+            return "Hoenn"
+        }
+        else if(region == "Sinnoh"){
+            return "Sinnoh"
+        }
+        else if(region == "Unova"){
+            return "Unova"
+        }
+        else if(region == "Kalos"){
+            return "Kalos"
+        }
+        else if(region == "Alola"){
+            return "Alola"
+        }
+        else if(region == "Galar"){
+            return "Galar"
+        }
+        else if(region == "Paldea"){
+            return "Paldea"
+        }
+        else{ //!currently for testing purposes. Should be "undisclosed"
+            return "Kanto"
+        }
     }
 
-//TODO pokemon trainer output
-function displayProfile(data){
-    console.log(data);
-    let userName = data.username;
-    console.log(userName);
-    let region = getRegion(data.bio);
-    console.log(region);
-    let id = CreateTrainer(userName);
-    console.log(id.id);
-    let money = getThatBag();
-    // console.log(money);
-    let pokedex = getPokedex();
-    // console.log(pokedex);
-
-    //adding text
-    const canvas = document.getElementById("trainerCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.font = "24px Roboto";
-    ctx.fillText("IDNo. " + id.id, 263, 42);
-    ctx.fillText("NAME: " + userName, 45, 84);
-    ctx.fillText("REGION: " + region, 45, 138);
-    ctx.fillText("MONEY: $" + money, 45, 170);
-    ctx.fillText("POKEDéX: " + pokedex, 45, 202);
-}
-
-function getRegion(region){
-    if(region == "Kanto"){
-        return "Kanto"
-    }
-    else if(region == "Johto"){
-        return "Johto"
-    }
-    else if(region == "Hoenn"){
-        return "Hoenn"
-    }
-    else if(region == "Sinnoh"){
-        return "Sinnoh"
-    }
-    else if(region == "Unova"){
-        return "Unova"
-    }
-    else if(region == "Kalos"){
-        return "Kalos"
-    }
-    else if(region == "Alola"){
-        return "Alola"
-    }
-    else if(region == "Galar"){
-        return "Galar"
-    }
-    else if(region == "Paldea"){
-        return "Paldea"
-    }
-    else{ //!for texting purposes. Should be "undisclosed"
-        return "Kanto"
-    }
-}
-
-
+//adding images to canvas
 function draw(ctx) {
     //coordinates based on card dimensions
-    let pokegridW = 28;
-    let pokegridH = 222;
-
-    let offsetW = 84;
-    let offsetH = 68;
+        let pokegridW = 28;
+        let pokegridH = 222;
+        let offsetW = 84;
+        let offsetH = 68;
     
-    const loginData = getLoginData();
-    let userCard = CreateTrainer(loginData.username);
-    console.log(userCard.pokemon1)
+    //pulling login data
+        const loginData = getLoginData();
+        let userCard = CreateTrainer(loginData.username);
+        console.log(userCard.pokemon1)
 
-    // adding pokemon
+    // adding pokemon images
+        //!can this be merged with line 47?
         let pokemon1 = loadImage("pokehashLibs/pokemons/pokemon" + userCard.pokemon1 + ".png");
         pokemon1.onload = function() {
             ctx.drawImage(pokemon1, pokegridW, pokegridH);
@@ -286,38 +311,62 @@ function draw(ctx) {
             ctx.drawImage(pokemon6, pokegridW + (offsetW*2), pokegridH + (offsetH));
         };
     
-    // adding trainer image
+    // adding trainer images
         let trainerImg = loadImage("pokehashLibs/trainers/trainer" + userCard.trainer + ".png");
         trainerImg.onload = function() {
             ctx.drawImage(trainerImg, 260, 170 )
         }
 
     //? adding badges
-    let horizontal = 52;
-    let offset = 48;
-    let badge = [];
+        //setting coordinates
+        let horizontal = 52;
+        let offset = 48;
 
-    for (let j = 0; j < 8; j++) {
-        badge[j] = loadImage('pokehashLibs/badges/kanto/kanto' + j + '.png');
-    }
-    
-    for (let j = 8; j < 16; j++) {
-        badge[j] = loadImage('pokehashLibs/badges/johto/johto' + j + '.png');
-    }
+        //establishing variables
+        const region = trainercard.region;
+        let regionoffset = 0;
 
-    let regionoffset = 0;
-    let region = ""
-    if(region == "Johto"){
-        regionoffset = 8;
-    }else{
-        regionoffset = 0;
-    }
-    for(let j = 0; j < 8; j++){
-        badge[j] = new Image();
-        badge[j].onload = function() {
-            ctx.drawImage(badge[j+regionoffset], horizontal + (j*offset), 380);
-        };
-    }
+        //creating conditions for loop
+        if(region == "Johto"){
+            regionoffset = 8;
+        }else{
+            regionoffset = 0;
+        }
+
+        //loading badges async
+            let pokeBadges = [];
+
+            for (let j = 0; j < 8; j++) {
+                pokeBadges.push(loadBadge(j + regionoffset, horizontal + (j*offset), 380, ctx));
+            }
+
+            Promise.all(pokeBadges).then(() => {
+                console.log("Badges loaded");
+            });
+            
+            function loadBadge(i, x, y, ctx) {
+                return new Promise((resolve, reject) =>{
+                    badge[i] = loadImage('./pokehashLibs/badges/kanto/kanto' + i + '.png');
+                    badge[i].onload = function() {
+                        ctx.drawImage(badge[i], x, y);
+                        resolve();
+                    };
+                });
+            }
+
+        // for (let j = 0; j < 8; j++) {
+        //     badge[j] = loadImage('pokehashLibs/badges/kanto/kanto' + j + '.png');
+        // }
+        
+        // for (let j = 8; j < 16; j++) {
+        //     badge[j] = loadImage('pokehashLibs/badges/johto/johto' + j + '.png');
+        // }
+        // for(let j = 0; j < 8; j++){
+        //     badge[j] = new Image();
+        //     badge[j].onload = function() {
+        //         ctx.drawImage(badge[j+regionoffset], horizontal + (j*offset), 380);
+        //     };
+        // }
 };
 
 function CreateTrainer(name) {
@@ -325,7 +374,7 @@ function CreateTrainer(name) {
         var hash = sha256(name);
         
         trainercard.name		= name;
-        // trainercard.region		= regions[	(parseInt('0x' + hash.substring(0, 4)) % 2)];
+        // trainercard.region		= getRegion(data.bio);
         // trainercard.hometown	= cities[		(parseInt('0x' + hash.substring(0, 4)) % 20)];
         trainercard.money		= 			(parseInt('0x' + hash.substring(5, 9)));
         trainercard.pokedex		= 			(parseInt('0x' + hash.substring(10, 14)) % 252);
@@ -441,3 +490,4 @@ function CreateTrainer(name) {
     
         return result;
     };
+
